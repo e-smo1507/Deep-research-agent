d19bbbb1a7d23200e702309dd5563e350b3bc736
e-smo1507 <esmoliguni@gmail.com>
2026-09-19 22:28:30 +0530

    fix: remove pyproject.toml to enable vercel automatic entrypoint detection

--- api/index.py
+++ api/index.py
@@ -1,8 +1,7 @@
 import os
 import sys
 
-# Add backend directory to sys.path
-backend_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "backend"))
+backend_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'backend'))
 if backend_dir not in sys.path:
     sys.path.insert(0, backend_dir)
 
